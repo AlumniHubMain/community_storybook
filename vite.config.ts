0999b7696bdfeb0a885a1f93b97c293c03bef121
Vladimir Alekseev <vcs@avs.guru>
2025-02-25 14:00:28 +0300

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {tamaguiPlugin} from "@tamagui/vite-plugin";
+import { tamaguiPlugin } from "@tamagui/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
