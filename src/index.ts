8e6b4203f7661181eeb7e9edf220b96171e6d463
Vladimir Alekseev <vcs@avs.guru>
2025-03-04 16:58:10 +0300

    added temp expo

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 export { default as config } from '../tamagui.config';
+
 export { Button } from './components/Button/Button';
-export { CustomToast } from './components/CustomToast';
+
 export { MyComponent } from './components/MyComponent';
+
+export { CustomToast } from './components/CustomToast';
 export { NativeToast } from './components/NativeToast';
+export { ToastViewport, useToastController, ToastProvider } from '@tamagui/toast';
+
 export { SwitchRouterButton } from './components/SwitchRouterButton';
 export { SwitchThemeButton } from './components/SwitchThemeButton';
-export { ToastProvider } from '@tamagui/toast';
