f65500ec0f1d0c6f6d760523688f967757608bde
Vladimir Alekseev <vcs@avs.guru>
2025-03-04 16:38:21 +0300

    added temp components

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 export { default as config } from '../tamagui.config';
 export { Button } from './components/Button/Button';
+export { CustomToast } from './components/CustomToast';
+export { MyComponent } from './components/MyComponent';
+export { NativeToast } from './components/NativeToast';
+export { SwitchRouterButton } from './components/SwitchRouterButton';
+export { SwitchThemeButton } from './components/SwitchThemeButton';
+export { ToastProvider } from '@tamagui/toast';
